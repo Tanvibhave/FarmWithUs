f6d2716d8b807fc5fe99743d99962e20ce015c02
Jason Gonsalo de Araujo <35160263+Gonzalo9977@users.noreply.github.com>
2020-08-14 21:30:38 +0530

    Add files via upload

---
+++ pages/footer.php
@@ -0,0 +1,10 @@
+ <footer class="footer" data-background-color="black">
+            <div class="container">
+                <div class="copyright">
+                    &copy;
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>, FarmWithUs
+                </div>
+            </div>
+        </footer>
---
+++ pages/session_script.php
@@ -0,0 +1,26 @@
+<?php 
+
+session_start();
+require '../config/dbconn.php';
+if (isset($_SESSION["supp_uname"])){
+
+
+	 $query=mysqli_query($dbconn,"SELECT * FROM `supplier` WHERE supp_uname='".$_SESSION['supp_name']."' AND supp_password='".$_SESSION['supp_password']."'");
+	  $res=mysqli_fetch_array($query);
+	
+
+		$supp_name =$_SESSION["supp_name"];
+		$supp_id =$_SESSION["id"];
+	
+		
+		// $user_type =$_SESSION["user_type"] ;
+		// $type_id= $_SESSION["type_id"] ;
+
+
+
+}else{
+
+	
+}
+
+?>
